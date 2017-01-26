336c90207cea13f0d0d82abe678569ed5373ff2b
David Carpus <David.Carpus@gmail.com>
2017-01-26 06:45:28 -0500

    Initial live reloading of browser when styles, php and HTML files change

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,15 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
-//    compass = require('gulp-compass'),
     concat = require('gulp-concat'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    browserSync = require('browser-sync').create();
 
 var coffeeSources = ['components/coffee/tagline.coffee']
 var jsSources = [
+  'components/scripts/tagline.js',
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
-  'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss']
@@ -29,22 +29,33 @@ gulp.task('js', function () {
     .pipe(gulp.dest('builds/development/js'))
 });
 
-/*gulp.task('compass', function () {
-  gulp.src(sassSources)
-    .pipe(compass({
-      sass: 'components/sass',
-      comments: true,
-      image: 'builds/development/images',
-      style: 'expanded'
-    })
-      .on('error', gutil.log))
-    .pipe(gulp.dest('builds/development/css'))
-});
-*/
 gulp.task('compass', function() {
    return sass(sassSources, {
      compass: true,
      lineNumbers: true
    }).on('error', gutil.log)
    .pipe(gulp.dest('builds/development/css'))
+   .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'browser-sync', 'watch']);
+
+gulp.task('watch', function () {
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(['builds/development/*.php', 'builds/development/*.html'])
+  .on('change', function(file) {
+      gutil.log(gutil.colors.yellow('PHP or HTML file changed' + ' (' + file.path + ')'));
+      browserSync.reload();
+  });
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+      proxy: "127.0.0.5:8080",
+      port: 8010,
+      open: true,
+      notify: false
+    });
 });
